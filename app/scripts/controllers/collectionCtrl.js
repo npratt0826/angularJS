77c7796ea09ccb6cad8701fe3f66ab5abab28bd1
npratt0826 <nicholasapratt@gmail.com>
2017-05-09 09:22:48 -0400

    not sure

--- app/scripts/controllers/collectionCtrl.js
+++ app/scripts/controllers/collectionCtrl.js
@@ -1,5 +1,4 @@
 (function() {
-<<<<<<< HEAD
     function CollectionCtrl(Fixtures) {
         this.albums = Fixtures.getCollection(12);
     }
@@ -8,13 +7,3 @@
         .module('blocJams')
         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
 })();
-=======
-     function CollectionCtrl(Fixtures) {
-       this.albums = Fixtures.getCollection(12);
-     }
-
-     angular
-         .module('blocJams')
-         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
- })();
->>>>>>> checkpoint-6-services
