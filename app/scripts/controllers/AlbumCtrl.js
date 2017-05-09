bbcbc8fbeca56ebd19ace21a3a4aced699c708b6
npratt0826 <nicholasapratt@gmail.com>
2017-05-09 09:10:26 -0400

    Collection view working but not album song list

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,14 +1,9 @@
 (function() {
-     function AlbumCtrl(Fixtures) {
-       this.albumData = Fixtures.getAlbum();
-       this.songs =[];
-       var songsLength = this.albumData.songs.length;
-       for(var i=0; i < songsLength; i++){
-          this.songs.push(this.albumData.songs[i]);
-       }
-     }
+    function AlbumCtrl (Fixtures) {
+        this.albumData = Fixtures.getAlbum();
+    }
 
-     angular
-         .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
- })();
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+})();
