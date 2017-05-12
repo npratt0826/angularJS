d2818460f45ecbc695cd8e9e2dd6dcb3046e32e0
npratt0826 <nicholasapratt@gmail.com>
2017-05-12 10:50:16 -0400

    assignment 11: changed timecode using buzz.toTimer

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,24 +1,29 @@
 (function () {
   function timecode() {
     return function(seconds){
-      var seconds = Number.parseFloat(seconds);
+    //   var seconds = Number.parseFloat(seconds);
+    //
+    //   if(Number.isNaN(seconds)){
+    //     return '-:--';
+    //   }
+    //
+    //   var wholeSeconds = Math.floor(seconds);
+    //   var minutes = Math.floor(wholeSeconds / 60);
+    //   var remainingSeconds = wholeSeconds % 60;
+    //
+    //   var output = minutes + ':';
+    //
+    //   if (remainingSeconds < 10) {
+    //      output += '0';
+    //   }
+    //
+    //   output += remainingSeconds;
+    //   return output;
 
-      if(Number.isNaN(seconds)){
-        return '-:--';
-      }
+    var timer = buzz.toTimer(seconds);
 
-      var wholeSeconds = Math.floor(seconds);
-      var minutes = Math.floor(wholeSeconds / 60);
-      var remainingSeconds = wholeSeconds % 60;
 
-      var output = minutes + ':';
-
-      if (remainingSeconds < 10) {
-         output += '0';
-      }
-
-      output += remainingSeconds;
-      return output;
+    return timer;
     };
 
   }
